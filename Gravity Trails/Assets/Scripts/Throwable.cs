978135f5834ce0e55f01cfd3945434efb9b24016
RAGHHHHHHHHHHHHHHHHHHHHH <markhamonca@codeninjas.com>
2025-05-16 17:50:28 -0400

    yea
    101

--- Gravity Trails/Assets/Scripts/Throwable.cs
+++ Gravity Trails/Assets/Scripts/Throwable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Throwable : MonoBehaviour
 {
@@ -8,25 +9,33 @@ public class Throwable : MonoBehaviour
     public int throwablecounter;
     public Vector3 offset;
     public Vector3 Direction;
+    public TMP_Text collectableCounter;
 
     void Update()
     {
-    throwablecounter =+ 1;
+        collectableCounter.text = throwablecounter.ToString();
         if (Input.GetButtonDown("Fire1"))
         {
-            offset = new Vector3(1, 0, 0);
-            offset = transform.localScale.x * new Vector3(1, 0, 0);
-            Vector3 throwablePosition = transform.position + offset;
-            Instantiate(ObgectThrown, throwablePosition, transform.rotation);
-            
+            if (throwablecounter != 0)
+            {
+                throwablecounter -= 1;
+                offset = new Vector3(1, 0, 0);
+                offset = transform.localScale.x * new Vector3(1, 0, 0);
+                Vector3 throwablePosition = transform.position + offset;
+                Instantiate(ObgectThrown, throwablePosition, transform.rotation);
+                
+            }
         }
-
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("GroundShuriken"))
+        {
+            throwablecounter += 1;
         }
     }
 }
