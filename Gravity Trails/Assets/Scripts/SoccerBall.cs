06976068d510a3fc5ebe7c15c357563980080f1b
RAGHHHHHHHHHHHHHHHHHHHHH <markhamonca@codeninjas.com>
2025-06-06 17:47:06 -0400

    goog
    Step MainMenu

--- Gravity Trails/Assets/Scripts/SoccerBall.cs
+++ Gravity Trails/Assets/Scripts/SoccerBall.cs
@@ -1,43 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SoccerBall : MonoBehaviour
 {
-    int goalNet1 = 0;
-    int goalNet2 = 0;
-    private Vector3 StartPos;
+    public int goalNet2 = 0;
+    public int goalNet1 = 0;
+
+    public GameObject P1;
+    public GameObject P2;   
+
+    public Rigidbody2D rb;
+    private Vector3 startPos;
+    private Vector3 startPosP1;
+    private Vector3 startPosP2;
+
+    public TMP_Text scoreText;
     //public Transform ogPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartPos = transform.position;
+        startPos = transform.position;
+        startPosP1 = P1.transform.position;
+        startPosP2 = P2.transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var P1Score = goalNet1;
+        var P2Score = goalNet2;
 
+        scoreText.text = ($"P1 Score:{P1Score}/9 P2 Score:{P2Score}/2");
+
+        if(goalNet1 == 1)
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if (goalNet2 == 2)
+        {
+            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collison)
     {
-        if (gameObject.CompareTag("GoalNet1"))
+        if (collison.gameObject.CompareTag("GoalNet1"))
         {
             goalNet1 += 1;
             RestartPoint();
         }
 
-        if (gameObject.CompareTag("GoalNet2"))
+        if (collison.gameObject.CompareTag("GoalNet2"))
         {
             goalNet2 += 1;
             RestartPoint();
         }
     }
 
-    private void RestartPoint()
+    public void RestartPoint()
     {
-        transform.position = StartPos; 
+        transform.position = startPos;
+        rb.velocity = new Vector3(0, 0, 0);
+        P1.transform.position = startPosP1;
+        P2.transform.position = startPosP2;
     }
 }
